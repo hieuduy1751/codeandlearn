6a22e3d6c785d00f4217a08317b008fc9165397a
DuyHieu <hieuduy1751@gmail.com>
2020-04-03 19:41:21 +0700

    section 3 and footer link completed

--- watchmorebut.js
+++ watchmorebut.js
@@ -1,3 +1,21 @@
+var textfunc = document.getElementsByClassName("text-func")[0];
+var textfunc1 = document.getElementsByClassName("text-func")[1];
+var trytext = document.querySelector("#keyboard-main + p");
+var keyboardmain = document.getElementById("keyboard-main");
+var rainbowtext = document.getElementById("rainbow-text");
+
+var textfuncr3 = document.getElementsByClassName("text-func-right-3")[0];
+var textfuncl3 = document.getElementsByClassName("text-func-left-3")[0];
+
+var imgspeedo = document.querySelector("img[alt='speedometer']");
+var imgukflag = document.querySelector("img[alt='ukflag']");
+
+var pleftbg31 = document.querySelector("img[alt='speedometer'] + p ");
+var pleftbg32 = document.querySelector("img[alt='speedometer'] + p + p");
+
+var prightbg31 = document.querySelector("img[alt='ukflag'] + p");
+var prightbg32 = document.querySelector("img[alt='ukflag'] + p + p");
+
 var watchmorebut = document.getElementById("watch-more-but");
 var gototop = document.getElementById("go-to-top");
 var n = 2;
@@ -9,6 +27,16 @@ watchmorebut.addEventListener("click", function() {
   rainbowtext.style.animationName = "rainbowtext";
   watchmorebut.href = "#background" + n;
   n++;
+  if (n == 4) {
+    textfuncr3.style.animationName = "textfunc3";
+    textfuncl3.style.animationName = "textfunc3";
+    imgspeedo.style.animationName = "imgbg3";
+    imgukflag.style.animationName = "imgbg3";
+    pleftbg31.style.animationName = "textdes3";
+    pleftbg32.style.animationName = "textdes3";
+    prightbg31.style.animationName = "textdes3";
+    prightbg32.style.animationName = "textdes3";
+  }
   if (n == 5) {
     n = 2;
     gototop.style.display = "unset";
@@ -19,9 +47,3 @@ gototop.addEventListener("click", function() {
   gototop.style.display = "none";
   watchmorebut.style.display = "unset";
 });
-
-var textfunc = document.getElementsByClassName("text-func")[0];
-var textfunc1 = document.getElementsByClassName("text-func")[1];
-var trytext = document.querySelector("#keyboard-main + p");
-var keyboardmain = document.getElementById("keyboard-main");
-var rainbowtext = document.getElementById("rainbow-text");
